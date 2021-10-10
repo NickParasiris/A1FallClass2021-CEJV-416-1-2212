4a68968e8c7cb9b24be9ab33c562b860c8474708
nicholasparasiris <nicholas.parasiris@mail.concordia.ca>
2021-10-10 14:48:03 -0400

    Finished Dinner order

--- src/Assignment_01/DinnerOrder.java
+++ src/Assignment_01/DinnerOrder.java
@@ -6,26 +6,43 @@ public class DinnerOrder {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int numberOfPeople;
+
         String response2 = "You are alone for dinner.";
         String response3 = "Please enter a proper value.";
 
-        while (true)
+        System.out.println("How many people are having dinner. \n(Enter a value between 1 and 100)");
+        if (s.hasNextInt())
         {
-            System.out.println("How many people are having dinner.");
             numberOfPeople = s.nextInt();
+            String response1 = "You have " + numberOfPeople + " people at the dinner table.";
+
             if (numberOfPeople > 1)
-            {
-                String response1 = "You have " + numberOfPeople + " people at the dinner table.";
                 System.out.println(response1);
-                break;
-            }
 
             if (numberOfPeople == 1)
-            {
                 System.out.println(response2);
-                break;
+
+        } else
+            numberOfPeople = -1;
+        s.nextLine();
+
+        while (numberOfPeople < 1 || numberOfPeople > 100)
+        {
+            System.out.println(response3);
+            if (s.hasNextInt())
+            {
+                numberOfPeople = s.nextInt();
+            } else
+            {
+                numberOfPeople = -1;
             }
+            s.nextLine();
         }
+
+
+
+
+
 
 
 
