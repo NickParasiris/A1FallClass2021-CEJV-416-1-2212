38000e57c2bf3d5fd4a3dd79cc6e59dbfb080181
nicholasparasiris <nicholas.parasiris@mail.concordia.ca>
2021-10-21 00:23:27 -0400

    Starting question 3

---
+++ src/Assignments/Assignment_01/Question_03/Rectangle.java
@@ -0,0 +1,22 @@
+package Assignments.Assignment_01.Question_03;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+
+//        Scanner s = new Scanner(System.in);
+//        int rectangleHeight = s.nextInt();
+//        int rectangleWidth = s.nextInt();
+//
+//        for (int x = 1; x <= rectangleHeight; x++)
+//        {
+//            System.out.println("*");
+//            for (int y = 1; y <= rectangleWidth; y++);
+//            {
+//                System.out.print("*");
+//            }
+//        }
+//
+    }
+}
---
+++ src/Assignments/Assignment_01/Question_03/Testing.java
@@ -0,0 +1,10 @@
+package Assignments.Assignment_01.Question_03;
+
+public class Testing {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
